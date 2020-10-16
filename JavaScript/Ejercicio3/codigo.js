96a22f0826ccccb70b0e7b12297e8650b1f4cc86
LuisAngelAlmanza <69695180+LuisAngelAlmanza@users.noreply.github.com>
2020-10-15 20:35:48 -0600

    Add files via upload

---
+++ JavaScript/Ejercicio3/codigo.js
@@ -0,0 +1,7 @@
+var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+
+
+for (i=0;i<=11;i++) { 
+   	alert(meses[i]);
+}
