42b67e4a11d5f5cfa54ff89a0e95d493be5db5de
LuisAngelAlmanza <69695180+LuisAngelAlmanza@users.noreply.github.com>
2020-10-22 21:18:14 -0600

    Add files via upload

---
+++ JavaScript/Ejercicio4/codigo.js
@@ -0,0 +1,22 @@
+var aux = [true, 5, false, "hola", "adios", 2];
+
+if (aux[3]>aux[4]) {
+	alert(aux[4]);
+}else{
+	alert(aux[3]);
+}
+
+var result_1 = aux[1] + aux[5];
+alert(result_1);
+
+var result_1 = aux[1] - aux[5];
+alert(result_1);
+
+var result_1 = aux[1] * aux[5];
+alert(result_1);
+
+var result_1 = aux[1] / aux[5];
+alert(result_1);
+
+var result_1 = aux[1] % aux[5];
+alert(result_1);
